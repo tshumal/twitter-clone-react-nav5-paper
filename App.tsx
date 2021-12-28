e277b82f3c2d8316457b75ff5663dad5014243ec
Lingani Tshuma <tshumal@gmail.com>
2021-12-28 07:42:35 +0200

    added components and data

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-import {RootNavigator} from "./src/rootNavigator";
+import {RootNavigator} from "./src/navigation/rootNavigator";
 
 export default function App() {
   return (
