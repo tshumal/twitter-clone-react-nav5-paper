1e6e683247a0025954a86f95ddf627bfd1c8e0e1
Lingani Tshuma <tshumal@gmail.com>
2021-12-28 07:17:53 +0200

    drawer navigation

--- src/navigation/rootNavigator.tsx
+++ src/navigation/rootNavigator.tsx
@@ -1,15 +1,10 @@
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import {Text, View} from "react-native";
+import {DrawerContent} from "./drawerContent";
 
 const Drawer = createDrawerNavigator();
 
-function DrawerContent() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Drawer content</Text>
-        </View>
-    );
-}
+
 
 function HomeScreen() {
     return (
@@ -21,7 +16,7 @@ function HomeScreen() {
 
 export const RootNavigator = () => {
     return (
-      <Drawer.Navigator drawerContent={ () => <DrawerContent />}>
+      <Drawer.Navigator drawerContent={ props => <DrawerContent {...props} />}>
           <Drawer.Screen name="Home" component={HomeScreen} />
       </Drawer.Navigator>
     );
